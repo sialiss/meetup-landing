d166efe6c099515b6ddd1714a10f4975b847bc40
Camellia <camellia.sialiss@gmail.com>
2026-07-10 15:08:50 +0300

    Updates

---
+++ _next/static/Pvs-pcculANSOa-COXH7O/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/meetup-landing//_next/:path+",
+        "destination": "/meetup-landing/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
